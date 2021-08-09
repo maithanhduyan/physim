f083e0d0f64bab718a115390283b2d91e31833bb
Mai Thành Duy An <tiachop0102@gmail.com>
2021-08-09 11:27:21 +0900

    Clock SVG animation.

--- src/js/main.js
+++ src/js/main.js
@@ -5,73 +5,92 @@
  * */
 
 console.log("WELCOME TO PHYSIM !");
-var width = 960,
-    height = 900;
+var width = 300,
+  height = 300;
 
-var nodes = d3.range(200).map(function() { return {radius: Math.random() * 12 + 4}; }),
-    root = nodes[0],
-    color = d3.scale.category10();
+const hour_hand_length = 0.45; // ratio to radius of clock
+const minute_hand_length = 0.7; // ratio to radius of clock
+const sec_hand_length = 0.9; // ratio to radius of clock
 
-root.radius = 0;
-root.fixed = true;
+var body = document.querySelector("body");
 
-var force = d3.layout.force()
-    .gravity(0.05)
-    .charge(function(d, i) { return i ? 0 : -2000; })
-    .nodes(nodes)
-    .size([width, height]);
+var container = document.createElement("div");
+container.setAttribute("style","width:999px; margin:0 auto;");
 
-force.start();
+body.appendChild(container);
 
-var svg = d3.select("body").append("svg")
-    .attr("width", width)
-    .attr("height", height);
+var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+svg.setAttribute("viewBox", "-1 -1 2 2");
+container.appendChild(svg);
 
-svg.selectAll("circle")
-    .data(nodes.slice(1))
-  .enter().append("circle")
-    .attr("r", function(d) { return d.radius; })
-    .style("fill", function(d, i) { return color(i % 3); });
+svg.setAttribute("width", width);
+svg.setAttribute("height", height);
 
-force.on("tick", function(e) {
-  var q = d3.geom.quadtree(nodes),
-      i = 0,
-      n = nodes.length;
+const circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+circle.setAttribute("cx", 0);
+circle.setAttribute("cy", 0);
+circle.setAttribute("r",.97);
+circle.setAttribute("style", "fill:none; stroke:black; stroke-width:2%");
 
-  while (++i < n) q.visit(collide(nodes[i]));
+svg.appendChild(circle);
 
-  svg.selectAll("circle")
-      .attr("cx", function(d) { return d.x; })
-      .attr("cy", function(d) { return d.y; });
+// draw second marks
+for (let ii = 0;ii < 60; ii++) {
+  const jj = 2 * Math.PI / 60 *ii;
+  const sec_pos = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+  sec_pos.setAttribute("cx", (Math.cos(jj)*.9) .toString ());
+  sec_pos.setAttribute("cy", (Math.sin(jj)*.9) .toString ());
+  sec_pos.setAttribute("r", "0.8%");
+  sec_pos.setAttribute("style", "fill:grey;stroke:none;");
+  svg.appendChild(sec_pos);
+};
+
+// draw hour marks
+for (let ii = 0;ii < 12; ii++) {
+  const jj = 2 * Math.PI / 12 *ii;
+  const hour_pos = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+  hour_pos.setAttribute("cx", (Math.cos(jj)*.9) .toString ());
+  hour_pos.setAttribute("cy", (Math.sin(jj)*.9) .toString ());
+  hour_pos.setAttribute("r", "3%");
+  hour_pos.setAttribute("style", "fill:black;stroke:none;");
+  svg.appendChild(hour_pos);
+};
+
+// hour hand
+const hour_hand = document.createElementNS("http://www.w3.org/2000/svg", "line");
+hour_hand.setAttribute("x1", "0");
+hour_hand.setAttribute("y1", "0");
+hour_hand.setAttribute("x2", "0");
+hour_hand.setAttribute("y2", (-1 * hour_hand_length) .toString ());
+hour_hand.setAttribute("style", "stroke:blue; stroke-width:5%;stroke-linecap:round");
+hour_hand.setAttribute("transform", "rotate(" + (((new Date()).getHours() % 12) * 30 + (new Date()).getMinutes() /2) +  ")");
+svg.appendChild(hour_hand);
+
+// minute hand
+const minute_hand = document.createElementNS("http://www.w3.org/2000/svg", "line");
+minute_hand.setAttribute("x1", "0");
+minute_hand.setAttribute("y1", "0");
+minute_hand.setAttribute("x2", "0");
+minute_hand.setAttribute("y2", (-1 * minute_hand_length) .toString ());
+minute_hand.setAttribute("style", "stroke:red; stroke-width:3%;stroke-linecap:round");
+minute_hand.setAttribute("transform", "rotate(" + ((new Date()).getMinutes()*6) +  ")");
+svg.appendChild(minute_hand);
+
+// second hand
+const sec_hand = document.createElementNS("http://www.w3.org/2000/svg", "line");
+sec_hand.setAttribute("x1", "0");
+sec_hand.setAttribute("y1", "0");
+sec_hand.setAttribute("x2", "0");
+sec_hand.setAttribute("y2", (-1 * sec_hand_length) .toString ());
+sec_hand.setAttribute("style", "stroke:black; stroke-width:1%;stroke-linecap:round");
+sec_hand.setAttribute("transform", "rotate(" + ((new Date()).getSeconds()*6) +  ")");
+svg.appendChild(sec_hand);
+
+const f_update_clock = (() => {
+  const dd = new Date();
+  sec_hand.setAttribute("transform", "rotate(" + (dd.getSeconds()*6) +  ")");
+  minute_hand.setAttribute("transform", "rotate(" + (dd.getMinutes()*6) +  ")");
+  hour_hand.setAttribute("transform", "rotate(" + ((dd.getHours() % 12) * 30 + dd.getMinutes() /2) +  ")");
 });
 
-svg.on("mousemove", function() {
-  var p1 = d3.mouse(this);
-  root.px = p1[0];
-  root.py = p1[1];
-  force.resume();
-});
-
-function collide(node) {
-  var r = node.radius + 16,
-      nx1 = node.x - r,
-      nx2 = node.x + r,
-      ny1 = node.y - r,
-      ny2 = node.y + r;
-  return function(quad, x1, y1, x2, y2) {
-    if (quad.point && (quad.point !== node)) {
-      var x = node.x - quad.point.x,
-          y = node.y - quad.point.y,
-          l = Math.sqrt(x * x + y * y),
-          r = node.radius + quad.point.radius;
-      if (l < r) {
-        l = (l - r) / l * .5;
-        node.x -= x *= l;
-        node.y -= y *= l;
-        quad.point.x += x;
-        quad.point.y += y;
-      }
-    }
-    return x1 > nx2 || x2 < nx1 || y1 > ny2 || y2 < ny1;
-  };
-}
+setInterval( f_update_clock , 1000);
